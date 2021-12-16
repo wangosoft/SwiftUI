b1cee7bfad81fc3dee3ea35e05803ce133efe5b7
U05381 <oguler@burgan.com.tr>
2021-12-16 22:32:40 +0300

    Initial Commit

---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Onur GÜLER on 16.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
